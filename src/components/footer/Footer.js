99474a244d1bf7fa83e8827704cba846ede29ff3
malonetheressa <theressadmalone@gmail.com>
2023-05-09 20:58:23 +0200

    :lipstick: added sass

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import '../../index.scss';
+
+function Footer() {
+    return (
+        <div>
+            <footer className="footer">
+                <p>© Theressa Malone, 2023</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import '../../index.scss';
+
+function Header() {
+    return (
+        <div>
+            <header className="header">
+                <p>Theressa Malone</p>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
