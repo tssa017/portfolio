399da9e2e1b898fd92bc31e4965d83b9ed870d88
malonetheressa <theressadmalone@gmail.com>
2023-05-09 20:58:05 +0200

    :art: re-configured pre-built project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// Imports
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.scss';
 
+// Components
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+// Function dynamically displays each page in the browser
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Header />
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
