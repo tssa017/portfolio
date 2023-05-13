355bc249651210541ab062cffd3f3ad2be8f5b82
malonetheressa <theressadmalone@gmail.com>
2023-05-14 00:18:38 +0200

    :lipstick: added avatar

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -4,15 +4,17 @@ import Button from '../button/Button.js';
 function Bio() {
     return (
         <div className="bio">
-            <p>
+            <img src="images/avatar.png" className="bio__img"></img>
+            <p className="bio__para">
                 I started learning to code so that I could optimise the layout
                 of my online literary magazine in HTML and CSS. I quickly became
                 interested in programming, and began to develop my portfolio
                 with Full-Stack projects.
             </p>
             <Button
-            // className={}
-            // onClick={}
+                className={`bio__btn`}
+                // onClick={}
+                href="mailto:theressadmalone@gmail.com"
             >
                 Say hi 👋
             </Button>
