4dce7d81ff4b634e11e87c655ddc6a175bd4475f
malonetheressa <theressadmalone@gmail.com>
2023-06-09 09:42:36 +0200

    :sparkles: downloadable cv

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -1,6 +1,7 @@
 import '../../index.scss';
 import LanguageContext from '../language/LanguageContext';
 import Paragraph from '../paragraph/Paragraph.js';
+import Button from '../button/Button';
 
 function Bio() {
     return (
@@ -26,6 +27,29 @@ function Bio() {
                             ? 'I started learning to code so that I could optimise the layout of my online literary magazine in HTML and CSS. I quickly became interested in programming, and began to develop my portfolio with Full-Stack projects. I am now looking to collaborate with a team of talented developers, building high quality, long term projects in the French Tech ecosystem.'
                             : "J'ai commencé à apprendre à coder afin d'optimiser la mise en page de mon magazine littéraire en ligne en HTML et CSS. Je me suis rapidement intéressée à la programmation et j'ai commencé à développer mon portefeuille avec des projets Full-Stack. Je cherche aujourd'hui à collaborer avec une équipe de développeurs talentueux, en construisant des projets de haute qualité et à long terme dans l'écosystème de la French Tech."}
                     </Paragraph>
+                    <div class="cv" id="cv">
+                        {isEnglishClicked ? (
+                            <a
+                                download=""
+                                href="pdf/CV_June_2023_compressed.pdf"
+                                aria-label="Link to Token to Theressa Malone CV"
+                            >
+                                <Button className="cv__btn">
+                                    Download my CV!
+                                </Button>
+                            </a>
+                        ) : (
+                            <a
+                                download=""
+                                href="pdf/CV_juin_2023_compressed.pdf"
+                                aria-label="Link to Token to Theressa Malone CV"
+                            >
+                                <Button className="cv__btn">
+                                    Télécharger mon CV !
+                                </Button>
+                            </a>
+                        )}
+                    </div>
                 </div>
             )}
         </LanguageContext.Consumer>
