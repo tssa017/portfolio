08a0857a5fdfe0789886c46ef02742af49a10f4e
malonetheressa <theressadmalone@gmail.com>
2023-06-12 17:21:28 +0200

    :art: txt update

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -19,13 +19,13 @@ function Bio() {
                     </h2>
                     <Paragraph className="bio__para">
                         {isEnglishClicked
-                            ? "Hi! I'm Theressa, a bilingual web developer on a mission to build accessible, responsive, and beautiful web applications ✨"
-                            : 'Salut ! Je suis Theressa, une développeuse web bilingue dont la mission est de créer des applications web accessibles, responsives et belles ✨'}
+                            ? "Hi! I'm Theressa, a curious and highly motivated web developer on a mission to build accessible, intuitive, and meaningful web applications ✨"
+                            : 'Salut ! Je suis Theressa, une développeuse web curieuse et très motivée dont la mission est de créer des applications web accessibles, intuitives et utiles ✨'}
                     </Paragraph>
                     <Paragraph className="bio__para">
                         {isEnglishClicked
-                            ? 'I started learning to code so that I could optimise the layout of my online literary magazine in HTML and CSS. I quickly became interested in programming, and began to develop my portfolio with Full-Stack projects. I am now looking to collaborate with a team of talented developers, building high quality, long term projects in the French Tech ecosystem.'
-                            : "J'ai commencé à apprendre à coder afin d'optimiser la mise en page de mon magazine littéraire en ligne en HTML et CSS. Je me suis rapidement intéressée à la programmation et j'ai commencé à développer mon portefeuille avec des projets Full-Stack. Je cherche aujourd'hui à collaborer avec une équipe de développeurs talentueux, en construisant des projets de haute qualité et à long terme dans l'écosystème de la French Tech."}
+                            ? 'I started learning to code so that I could optimise the layout of my online literary magazine in HTML and CSS. I quickly became interested in programming, and graduated with my diploma in Web Development from OpenClassrooms. I am now looking to collaborate with a team of talented developers, building high quality, long term projects in the French Tech ecosystem.'
+                            : "J'ai commencé à apprendre à coder afin d'optimiser la mise en page de mon magazine littéraire en ligne en HTML et CSS. Je me suis rapidement intéressée à la programmation, et j'ai obtenu mon diplôme en développement web chez OpenClassrooms. Je cherche maintenant à collaborer avec une équipe de développeurs talentueux, en construisant des projets de qualité et à long terme dans l'écosystème de la French Tech."}
                     </Paragraph>
                     <div class="cv" id="cv">
                         {isEnglishClicked ? (
