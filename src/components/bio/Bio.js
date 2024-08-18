086ad736353017d50339140231493aa056168384
malonetheressa <theressadmalone@gmail.com>
2024-08-18 12:56:45 +0200

    :memo: update info and pic

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -19,13 +19,8 @@ function Bio() {
                     </Paragraph>
                     <Paragraph className="bio__para">
                         {isEnglishClicked
-                            ? 'While taking computer science courses with OpenClassrooms, I started working as a freelance web dev. I designed and built new business websites, and performed audits to improve web performance and SEO on existing websites.'
-                            : "Pendant que je suivais des cours d'informatique sur OpenClassrooms, j'ai commencé à travailler comme développeuse web en freelance. J'ai conçu et créé de nouveaux sites web pour des entreprises, et j'ai réalisé des audits pour améliorer la performance et le SEO des sites existants."}
-                    </Paragraph>
-                    <Paragraph className="bio__para">
-                        {isEnglishClicked
-                            ? 'I soon landed an apprenticeship as a back-end developer in a Big Data start-up. While working there, I took an interest in the overall product lifecycle. Taking on Product Management responsibilities, I leveraged the opportunity to combine my design and accessibility experience with my technical expertise. I leapt into building a full-time career in Product Management.'
-                            : "J'ai rapidement décroché un apprentissage en tant que développeuse back-end dans une start-up spécialisée dans le Big Data. Pendant que je travaillais là-bas, je me suis intéressé au cycle de vie complet des produits. En assumant des responsabilités en gestion de produit, j'ai saisi l'opportunité de combiner mon expérience en design et accessibilité avec mes compétences techniques. Je me suis lancé à plein temps dans une carrière en gestion de produit."}
+                            ? 'I landed an apprenticeship as a back-end developer in a Big Data start-up. While working there, I took an interest in the overall product lifecycle. Taking on Product Management responsibilities, I leveraged the opportunity to combine my design and accessibility experience with my technical expertise. I leapt into building a full-time career in Product Management.'
+                            : "J'ai décroché un apprentissage en tant que développeuse back-end dans une start-up spécialisée dans le Big Data. Pendant que je travaillais là-bas, je me suis intéressé au cycle de vie complet des produits. En assumant des responsabilités en gestion de produit, j'ai saisi l'opportunité de combiner mon expérience en design et accessibilité avec mes compétences techniques. Je me suis lancé à plein temps dans une carrière en gestion de produit."}
                     </Paragraph>
                     <Paragraph className="bio__para">
                         {isEnglishClicked
